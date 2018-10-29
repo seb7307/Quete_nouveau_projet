0926286cd10b8138e37d0530c182d5d690f8ac5a
seb7307 <boutigny.sebastien73@gmail.com>
2018-10-29 15:23:25 +0100

    symphony2

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number")
+     */
+
+    public function number()
+    {
+        $number = index(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+
+}
